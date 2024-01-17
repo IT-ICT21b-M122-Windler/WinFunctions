b9197159e4d135e5967e417c8e553d206faec52e
Nils <nils.windler.23@gmail.com>
2024-01-17 17:09:49 +0100

    Worked on Function Set-Language

--- WinFunctions.psm1
+++ WinFunctions.psm1
@@ -17,39 +17,48 @@ function Set-Language {
     param (
         #Sprache
         [Parameter(Mandatory = $false, HelpMessage = "Set the Language and Format on Windows (de-CH, en-US etc.)")]
-        [string]$DisplayLanguage = "",
+        [string]$DisplayLanguage = "Invalid", #Ivalid do while loop
 
         [Parameter(Mandatory = $false, HelpMessage = "Set the Keyboard Language on Windows")]
-        [string]$KeyboardLanguage = "",
+        [string]$KeyboardLanguage = "Invalid", #Ivalid do while loop
         [Parameter(Mandatory = $false, HelpMessage = "Set the Keyboard Layout on Windows")]
-        [string]$KeyboardLayout = "",
+        [string]$KeyboardLayout = "Invalid", #Ivalid do while loop
 
         #Region
         [Parameter(Mandatory = $false, HelpMessage = "Set the Region on Windows")]
         [string]$Region= "Invalid", #Ivalid do while loop
         [Parameter(Mandatory = $false, HelpMessage = "Set the Reginal Format on Windows (de-CH, en-US etc.)")]
-        [string]$RegionalFormat = "",
+        [string]$RegionalFormat = "Invalid", #Ivalid do while loop
 
         #Format
         [Parameter(Mandatory = $false, HelpMessage = "Set the Time Format on Windows")]
         [ValidateSet("Long dd/MM/yyyy", "Short dd/MM/yy", "USLong yyyy/MM/dd", "USShort yy/MM/dd")]
-        $ShortDateFormat = "",
+        $ShortDateFormat = "Invalid", #Ivalid do while loop
         [Parameter(Mandatory = $false, HelpMessage = "Set the Time Format on Windows")]
         [ValidateSet("Long dd/MM/yyyy", "Short dd/MM/yy", "USLong yyyy/MM/dd", "USShort yy/MM/dd")]
-        $LongDateFormat = "",
+        $LongDateFormat = "Invalid", #Ivalid do while loop
         [Parameter(Mandatory = $false, HelpMessage = "Set the Time Format on Windows")]
         [ValidateSet(".", ",")]
-        $DecimalSeperator = ""
+        $DecimalSeperator = "Invalid" #Ivalid do while loop
         #etc nach Kundenwunsch...
     )
+
+    # ---------------- Protokollierung ----------------
+    # Transkript file
+    Start-Transcript -Path ".\set-Language-transcript.txt"
+    # Log file
+    $ActualDate = Get-Date -Format "yyyy-MM-dd_HH:mm:ss"
+    $LogOutputpath =  ".\" + "$ActualDate" + "_function-set-language.log"
+    Write-Log "Start logging" -File $LogOutputpath
 
     # ---------------- Variablen und Module ----------------
     Import-Module international
     $RegPath = "HKCU:\Control Panel\International"
-    $ActualDate = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
-    $Outputpath =  ".\" + "$ActualDate" + "changelanguage.log"
+    Write-Log "Import-Module international" -File $LogOutputpath
+    Write-Log "RegPath: $RegPath" -File $LogOutputpath
 
-    # Write-Host - Your curent Language settings
+    # ---------------- Getting current Informations ----------------
+    # Output - Your curent Language settings
     Write-Host "Your current Language and Format settings"
     Write-Host "Display Language: $(Get-WinUILanguageOverride).LanguageTag"
     Write-Host "Keyboard Language: $(Get-WinUserLanguageList).LanguageTag"
@@ -59,39 +68,35 @@ function Set-Language {
     Write-Host "Time Format short: $(Get-ItemProperty -Path HKCU:\Control Panel\International).sShortDate"
     Write-Host "Time Format long: $(Get-ItemProperty -Path HKCU:\Control Panel\International).sLongDate"
     Write-Host "Decimal Seperator: $(Get-ItemProperty -Path HKCU:\Control Panel\International).sDecimal"
-    # Write-Log - Your curent Language settings
-    Write-Log -Message "Your current Language and Format settings" -File $Outputpath
-    Write-Log -Message "Display Language: $(Get-WinUILanguageOverride).LanguageTag" -File $Outputpath
-    Write-Log -Message "Keyboard Language: $(Get-WinUserLanguageList).LanguageTag" -File $Outputpath
-    Write-Log -Message "Keyboard Layout: $(Get-WinUserLanguageList).InputMethodTips" -File $Outputpath
-    Write-Log -Message "Region: $(Get-WinHomeLocation).GeoId" -File $Outputpath
-    Write-Log -Message "Regional Format: $(Get-Culture).Name" -File $Outputpath
-    Write-Log -Message "Time Format short: $(Get-ItemProperty -Path HKCU:\Control Panel\International).sShortDate" -File $Outputpath
-    Write-Log -Message "Time Format long: $(Get-ItemProperty -Path HKCU:\Control Panel\International).sLongDate" -File $Outputpath
-    Write-Log -Message "Decimal Seperator: $(Get-ItemProperty -Path HKCU:\Control Panel\International).sDecimal" -File $Outputpath
+    # Log - Your curent Language settings
+    Write-Log -Message "Your current Language and Format settings" -File $LogOutputpath
+    Write-Log -Message "Display Language: $(Get-WinUILanguageOverride).LanguageTag" -File $LogOutputpath
+    Write-Log -Message "Keyboard Language: $(Get-WinUserLanguageList).LanguageTag" -File $LogOutputpath
+    Write-Log -Message "Keyboard Layout: $(Get-WinUserLanguageList).InputMethodTips" -File $LogOutputpath
+    Write-Log -Message "Region: $(Get-WinHomeLocation).GeoId" -File $LogOutputpath
+    Write-Log -Message "Regional Format: $(Get-Culture).Name" -File $LogOutputpath
+    Write-Log -Message "Time Format short: $(Get-ItemProperty -Path HKCU:\Control Panel\International).sShortDate" -File $LogOutputpath
+    Write-Log -Message "Time Format long: $(Get-ItemProperty -Path HKCU:\Control Panel\International).sLongDate" -File $LogOutputpath
+    Write-Log -Message "Decimal Seperator: $(Get-ItemProperty -Path HKCU:\Control Panel\International).sDecimal" -File $LogOutputpath
 
-
-
-    $CurrentLanguage = Get-WinUILanguageOverride
-    $CurrentLanguage = $CurrentLanguage.LanguageTag
-
+    # ---------------- Start, Getting Input ----------------
     Write-Host "Set the Language on Windows"
-    Write-Log -Message "Set the Language on Windows" -File $Outputpath
+    Write-Log -Message "Set the Language on Windows" -File $LogOutputpath
 
-    while ($Land -eq "Invalid"){
-        $Land = Read-Host "Set Region on Windows, Native Name or English Name"
-    }
-    while ($RegionalFormat -eq "Invalid"){
-        $RegionalFormat = Read-Host "Set Regional Format on Windows, Format: de-CH, en-US etc."
-    }
-    while ($Language -eq "Invalid"){
-        $Language = Read-Host "Set Language on Windows, Format: de-CH, en-US etc."
+    while ($DisplayLanguage -eq "Invalid"){
+        $DisplayLanguage = Read-Host "Set the Language on Windows, Format: de-CH, en-US etc."
     }
     while ($KeyboardLanguage -eq "Invalid"){
         $KeyboardLanguage = Read-Host "Set Keyboard Language on Windows, Format: Deutsch (Schweiz) or English (United States)"
     }
     while ($KeyboardLayout -eq "Invalid"){
         $KeyboardLayout = Read-Host "Set Keyboard Layout on Windows, Format: Deutsch (Schweiz) or English (United States)"
+    }
+    while ($Region -eq "Invalid"){
+        $Region = Read-Host "Set Region on Windows, Native Name or English Name"
+    }
+    while ($RegionalFormat -eq "Invalid"){
+        $RegionalFormat = Read-Host "Set Regional Format on Windows, Format: de-CH, en-US etc."
     }
     while ($ShortDateFormat -eq "Invalid"){
         $ShortDateFormat = Read-Host "Set Short Date Format on Windows, Format: Short dd/MM/yy"
@@ -104,10 +109,11 @@ function Set-Language {
     }
 
     # ---------------- Find GeoID ----------------
-    if ($Land -ne "") {
+    if ($Region -ne "") {
         $FindGeoID = "false"
         while ($FindGeoID -eq "false"){
             Write-Host "Searching GeoID..."
+            Wrtite-log -Message "Searching GeoID..." -File $LogOutputpath
             # Zugriff auf die CultureInfo-Klasse des .NET Frameworks, um alle Kulturen zu erhalten
             $cultures = [System.Globalization.CultureInfo]::GetCultures([System.Globalization.CultureTypes]::AllCultures)
             foreach ($culture in $cultures) { # Durchsuche alle Kulturen, um die entsprechende zu finden
@@ -126,11 +132,13 @@ function Set-Language {
                 # Erstelle ein RegionInfo-Objekt für den gefundenen Ländercode und speichere diesen in $GeoID
                 $regionInfo = New-Object System.Globalization.RegionInfo $landCode
                 $GeoID = $regionInfo.GeoId
-                Write-Host "$($Land): GeoID ist $GeoID"
+                Write-Host "$($Region): GeoID ist $GeoID"
+                Write-Log -Message "$($Region): GeoID ist $GeoID" -File $LogOutputpath
                 $FindGeoID = "true"
             }
             else { # Falls kein Ländercode gefunden wurde, gib eine Fehlermeldung aus
                 Write-Host "$($Land): Keine GeoID gefunden"
+                Write-Log -Message "$($Land): Keine GeoID gefunden" -File $LogOutputpath
                 $Land = Read-Host "Set Region on Windows, Native Name or English Name"
                 $FindGeoID = "false"
             }
@@ -192,13 +200,13 @@ function Set-Language {
     Write-Host "DecimalSeperator: $DecimalSeperator"
 
     # Ausgabe der gefundenen Informationen in eine Log-Datei
-    Write-Log -Message "Your Input" -File $Outputpath
-    Write-Log -Message "Language: $Language" -File $Outputpath
-    Write-Log -Message "Region: $GeoID ($Land, $($regionInfo.DisplayName))" -File $Outputpath
-    Write-Log -Message "Keyboard Language $KeyboardLanguage and Layout $KeyboardLayout : $imeCode" -File $Outputpath
-    Write-Log -Message "ShortDateFormat: $ShortDateFormat" -File $Outputpath
-    Write-Log -Message "LongDateFormat: $LongDateFormat" -File $Outputpath
-    Write-Log -Message "DecimalSeperator: $DecimalSeperator" -File $Outputpath
+    Write-Log -Message "Your Input" -File $LogOutputpath
+    Write-Log -Message "Language: $Language" -File $LogOutputpath
+    Write-Log -Message "Region: $GeoID ($Land, $($regionInfo.DisplayName))" -File $LogOutputpath
+    Write-Log -Message "Keyboard Language $KeyboardLanguage and Layout $KeyboardLayout : $imeCode" -File $LogOutputpath
+    Write-Log -Message "ShortDateFormat: $ShortDateFormat" -File $LogOutputpath
+    Write-Log -Message "LongDateFormat: $LongDateFormat" -File $LogOutputpath
+    Write-Log -Message "DecimalSeperator: $DecimalSeperator" -File $LogOutputpath
 
     $Confirm = Read-Host "Do you want to continue? (y/n)"
     if ($Confirm -eq "y") {
@@ -246,6 +254,9 @@ function Set-Language {
         Set-ItemProperty -Path $RegPath -Name $_ -Value $formatKeys[$_]
     }
     Write-Host "`nSuccessfully changed the language settings!"
+
+    # Stoppt die Protokollierung
+    Stop-Transcript
 }
 function Set-Taskbar {
     param (
